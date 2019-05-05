29d799ab3a624b88693058b33e17cae29dabfddd
Cory Eighan <zephyr834@gmail.com>
2019-05-05 15:56:00 -0700

    Created Post and User Model

---
+++ src/main/java/app/models/User.java
@@ -0,0 +1,57 @@
+package app.models;
+
+import java.text.MessageFormat;
+import java.util.HashSet;
+import java.util.Set;
+
+public class User {
+
+    private Long id;
+    private String username;
+    private String passwordHash;
+    private String fullName;
+    private Set<Post> posts = new HashSet<>();
+
+    User(Long id, String username, String fullName) {
+        this.id = id;
+        this.username = username;
+        this.fullName = fullName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    private void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    @Override
+    public String toString() {
+        return MessageFormat.format("Id: {0}, Username: {1}, Full Name: {2}", id, username, fullName);
+    }
+}
